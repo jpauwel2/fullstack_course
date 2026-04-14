94ffaa1fd87bcb5175f549691b780e49681c524a
jpauwel2 <jonas.pauwels@azr.be>
2026-04-14 15:14:56 +0200

    part1 exc 1.2

--- part1/courseinfo/part1-courseinfo/src/App.jsx
+++ part1/courseinfo/part1-courseinfo/src/App.jsx
@@ -4,10 +4,19 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return(<p>
     {props.part} {props.exercises}
   </p>)
+}
+
+const Content = (props) => {
+  return(
+  <div>
+    <Part part={props.parts[0]} exercises={props.exercises[0]}/>
+    <Part part={props.parts[1]} exercises={props.exercises[1]}/>
+    <Part part={props.parts[2]} exercises={props.exercises[2]}/>
+  </div>)
 }
 
 const Total = (props) => {
@@ -28,9 +37,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]}/>
       <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
