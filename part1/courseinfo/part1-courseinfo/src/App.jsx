4bc8f3f6f1509ba9deae8599c4885a29f02e30d1
jpauwel2 <jonas.pauwels@azr.be>
2026-06-12 13:54:28 +0200

    until part1 Javascript ex 1-5

--- part1/courseinfo/part1-courseinfo/src/App.jsx
+++ part1/courseinfo/part1-courseinfo/src/App.jsx
@@ -1,28 +1,28 @@
-const Header = (props) => {
-  console.log(props)
+const Header = ({course}) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{course}</h1>
   )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
+  const {name, exercises} = part
   return(<p>
-    {props.part.name} {props.part.exercises}
+    {name} {exercises}
   </p>)
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return(
   <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
   </div>)
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
   return(<p>
-    Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}
+    Number of exercises {parts[0].exercises+parts[1].exercises+parts[2].exercises}
   </p>)
 }
 
